343beccc850b55ed8e5d366a9841bc29812f5cb6
mky-cyber <weiliangying31@gmail.com>
2019-11-20 11:53:36 +1000

    Updates

---
+++ precache-manifest.d9c6583cee0d6b0323819a9701533a0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9196f03d2e142a08e5a49423e10913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b57e35129fef010c86a5",
+    "url": "/static/css/2.46f0bf41.chunk.css"
+  },
+  {
+    "revision": "b57e35129fef010c86a5",
+    "url": "/static/js/2.3f3eb526.chunk.js"
+  },
+  {
+    "revision": "d2cf5adadedde3768f84",
+    "url": "/static/js/main.5cfa3b04.chunk.js"
+  },
+  {
+    "revision": "4adf4b27496c67268970",
+    "url": "/static/js/runtime-main.080cc58b.js"
+  }
+]);
